eda925218b9ae91a4c58181098f3053b55248e4f
WebDragon <117004849+webdragon63@users.noreply.github.com>
2024-11-01 14:14:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,9 +13,9 @@ GREEN='\033[0;32m'
 echo -e "${VERMILION}Installing ..."
 cp -r src stegbrutex.sh /usr/local/bin/
 ln -sf /usr/local/bin/stegbrutex.sh /usr/local/bin/stegbrutex
-chmod +x /usr/local/bin/stegbrutex.sh
+chmod 777 /usr/local/bin/stegbrutex.sh
 chmod 777 /usr/local/bin/src
-chmod +x /usr/local/bin/src/main.py
+chmod 777 /usr/local/bin/src/main.py
 sleep 1
 echo -e "${WHITE}Congratulations installation complete ..."
 echo -e "${GREEN}Run this tool with stegbrutex command. Happy cracking"
