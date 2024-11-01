3b2ff2db58ca8f9ec2029bcabc286836bafb8051
WebDragon <117004849+webdragon63@users.noreply.github.com>
2024-11-01 14:02:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#!/bin/bash
+sleep 1
+VERMILION='\033[0;31m'
+if [[ $(id -u) -ne 0 ]] ; then 
+echo -e "${VERMILION}You are Not Root! Please Run as Root."
+echo -e "${VERMILION}Installation failed ..." ; exit 1 ; 
+fi
 VERMILION='\033[0;31m'
 WHITE='\033[0;37m'
 GREEN='\033[0;32m'
@@ -9,5 +16,5 @@ ln -sf /usr/local/bin/stegbrutex.sh /usr/local/bin/stegbrutex
 chmod +x /usr/local/bin/stegbrutex.sh
 chmod +x /usr/local/bin/src/main.py
 sleep 1
-echo -e "${WHITE}Congratulations installation completed ..."
+echo -e "${WHITE}Congratulations installation complete ..."
 echo -e "${GREEN}Run this tool with stegbrutex command. Happy cracking"
